446ab4aa5a9ac74019c84f4cebac75bb2ccf06df
Bohdan <protzzz.it@gmail.com>
2024-04-17 22:21:53 +0200

    Initial Commit

---
+++ ukol4-tmap-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ukol4-tmap-demo
+//
+//  Created by protzzz on 17.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ukol4-tmap-demo/ukol4_tmap_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ukol4_tmap_demoApp.swift
+//  ukol4-tmap-demo
+//
+//  Created by protzzz on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ukol4_tmap_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
