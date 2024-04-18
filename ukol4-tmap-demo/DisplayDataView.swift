20df230c599936f9f89d87428b4a20b963da5d5f
Bohdan <protzzz.it@gmail.com>
2024-04-18 13:49:42 +0200

    A finished project.

---
+++ ukol4-tmap-demo/DisplayDataView.swift
@@ -0,0 +1,8 @@
+//
+//  DisplayDataView.swift
+//  ukol4-tmap-demo
+//
+//  Created by protzzz on 18.04.2024.
+//
+
+import Foundation
---
+++ ukol4-tmap-demo/FormView.swift
@@ -0,0 +1,8 @@
+//
+//  FormView.swift
+//  ukol4-tmap-demo
+//
+//  Created by protzzz on 18.04.2024.
+//
+
+import Foundation
